71caf4d16ef31a686824d404e96337ba706b6008
YongZhi1990 <70925339+YongZhi1990@users.noreply.github.com>
2022-12-29 13:43:22 +0800

    Update safelist_sample_script.py

--- safelist_sample_script.py
+++ safelist_sample_script.py
@@ -16,35 +16,31 @@ async def request_executor(session, number, res_writer, err_writer,limit):
                 response_text = await response.text()
                 print(f'{number}, {time.asctime(time.localtime(time.time()))}, {response_text}')
                 await write_result([f'{number} : {response_text}'],res_writer)
-                print('after csv write')
                 await asyncio.sleep(4)
-                print(f'{number} after sleep') #sleep for this amount of seconds to achieve RPS (2 request per 4 seconds)
         except Exception as e:
             print(number, 'ERROR', str(e))
             await write_error([f'{number} and ERROR: {e}'], err_writer)
-            await asyncio.sleep(4) #sleep for this amount of seconds to achieve RPS (2 request per 4 seconds) - error calls are counted as request towards rate limiting too
+            await asyncio.sleep(4)
             return False
 
 
 async def write_result(result, writer):
-    async with asyncio.Lock():   # lock for gracefully write to shared file object
+    async with asyncio.Lock():  
         writer.writerow(result)
 
 async def write_error(error, writer):
-    async with asyncio.Lock(): # lock for gracefully write to shared file object
+    async with asyncio.Lock():
         writer.writerow(error)
 
 
 async def main():
     limit = asyncio.Semaphore(2) #limit to 2 concurrent request per on the created session because we are sleeping 4 second per request, this will be 2 request/ 4 second or 30 request per minute
   
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session: #opening up a session to use for the request
-        with open('res.csv', 'a') as csv_res, open('err.csv', 'a') as csv_err, open('whitelist.csv') as whitelist_csv:
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+        with open('res.csv', 'a') as csv_res, open('err.csv', 'a') as csv_err, open('~REPLACE WITH YOUR WHITELIST CSV FILENAME/FILEPATH') as whitelist_csv:
             res_writer = csv.writer(csv_res)
             err_writer = csv.writer(csv_err)
             request_builder = [request_executor(session, number.strip(), res_writer, err_writer, limit) for number in whitelist_csv]
-            #print(request_builder)
-            #print(type(request_builder))
             await asyncio.gather(*request_builder)
             print('!--- finished processing')
 
