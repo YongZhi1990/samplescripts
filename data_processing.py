fe0b955891e95fd4719d8fbae8b3a5d9d9b22a83
YongZhi1990 <70925339+YongZhi1990@users.noreply.github.com>
2023-03-27 09:40:49 +0800

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -39,3 +39,52 @@
 #    with open(f"application_error_log.csv",'a', newline="") as j:
 #        application_error_writer = csv.writer(j, delimiter=',')
 #        application_error_writer.writerow([datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S'), e])
+
+
+import asyncio
+import aiohttp
+import time
+import csv
+
+
+URL = 'https://verify.twilio.com/v2/SafeList/Numbers'
+ACCOUNT_SID = '~REPLACE WITH YOUR TWILIO ACCOUNT SID'
+AUTH_TOKEN = '~REPLACE WITH YOUR TWILIO AUTH TOKEN'
+
+async def request_executor(session, number, res_writer, err_writer,limit):
+    
+    async with limit:
+        try:
+            async with session.request(url=URL, method = 'POST', data={'PhoneNumber': number}, headers={"content-type": "application/x-www-form-urlencoded"}, auth=aiohttp.BasicAuth(ACCOUNT_SID, AUTH_TOKEN)) as response:
+                response_text = await response.text()
+                print(f'{number}, {time.asctime(time.localtime(time.time()))}, {response_text}')
+                await write_result([f'{number} : {response_text}'],res_writer)
+                await asyncio.sleep(4)
+        except Exception as e:
+            print(number, 'ERROR', str(e))
+            await write_error([f'{number} and ERROR: {e}'], err_writer)
+            await asyncio.sleep(4)
+            return False
+
+
+async def write_result(result, writer):
+    async with asyncio.Lock():  
+        writer.writerow(result)
+
+async def write_error(error, writer):
+    async with asyncio.Lock():
+        writer.writerow(error)
+
+
+async def main():
+    limit = asyncio.Semaphore(2) #limit to 2 concurrent request per on the created session because we are sleeping 4 second per request, this will be 2 request/ 4 second or 30 request per minute
+  
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+        with open('res.csv', 'a') as csv_res, open('err.csv', 'a') as csv_err, open('~REPLACE WITH YOUR WHITELIST CSV FILENAME/FILEPATH') as whitelist_csv:
+            res_writer = csv.writer(csv_res)
+            err_writer = csv.writer(csv_err)
+            request_builder = [request_executor(session, number.strip(), res_writer, err_writer, limit) for number in whitelist_csv]
+            await asyncio.gather(*request_builder)
+            print('!--- finished processing')
+
+asyncio.run(main())
